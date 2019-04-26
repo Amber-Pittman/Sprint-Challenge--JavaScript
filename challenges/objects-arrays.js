2730803cc692a562ec47350514f052d4bbdfc6b8
Amber Pittman <amber@amberpittman.com>
2019-04-26 19:35:58 -0400

    Created Uni Array

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -83,8 +83,11 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
-console.log(contactInfo);
 
+for (let i = 0; i < graduates.length; i++) {
+  contactInfo.push(graduates[i]["first_name"] + " " + graduates[i]["email"]);
+}
+console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
