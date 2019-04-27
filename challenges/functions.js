eb075b28df1eb244ef25cdb1b031a257d6509359
Amber Pittman <amber@amberpittman.com>
2019-04-26 20:00:53 -0400

    Created 3 CBs for consume()

--- challenges/functions.js
+++ challenges/functions.js
@@ -15,12 +15,14 @@ function consume(param1, param2, callback) {
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
+const add = (x, y) => x + y;
+const multiply = (x, y) => x * y;
+const greeting = (firstName, lastName) => `Hello ${firstName} ${lastName}, nice to meet you!`
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
